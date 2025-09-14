13c00d689457e4aa2d080503409f6686f8369e9a
torys877 <torylandua@gmail.com>
2025-09-14 19:33:31 +0200

    wip

--- pkg/types/embedder.go
+++ pkg/types/embedder.go
@@ -3,6 +3,6 @@ package types
 import "context"
 
 type Embedder interface {
-	Embed(ctx context.Context, msg string) ([]float32, error)
 	Name() string
+	Embed(ctx context.Context, msg string) ([]float32, error)
 }
--- pkg/types/entity.go
+++ pkg/types/entity.go
@@ -1,15 +1,11 @@
 package types
 
 type Entity struct {
-	ID      [16]byte
+	//ID      [16]byte
+	ID      string
 	UUID    string
 	Text    string
 	Payload map[string]string
 	Vector  []float32
 	Err     error
-}
-
-type ErrorEntity struct {
-	Entity
-	Err error
 }
